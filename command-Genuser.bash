2613a84f84283236f45b9166a55486c661c83f08
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-06-18 19:39:38 +0530

    Delete command-Genuser.bash

--- command-Genuser.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-loc=$(locate -i genuser.bash) #Locating the file after downloading
-x=$(echo $loc | sed s/genuser.bash/''/g) #getting only the directory of the file
-cd $x
-sudo chmod 770 $loc
-sudo sh -c "echo "alias genUser='./genuser.bash'" >> .bashrc"
