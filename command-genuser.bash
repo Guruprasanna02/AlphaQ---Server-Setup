3b9aff6a70d27581d8f5540638bcb8e55e5c417c
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-06-18 18:03:08 +0530

    Update command-genuser.bash

--- command-genuser.bash
+++ command-genuser.bash
@@ -2,6 +2,6 @@
 
 loc=$(locate -i genuser.bash) #Locating the file after downloading
 x=$(echo $loc | sed s/genuser.bash/''/g) #getting only the directory of the file
-sudo cd $x
+cd $x
 sudo chmod 770 $loc
 sudo sh -c "echo "alias genUser='./genuser.bash'" >> .bashrc"
