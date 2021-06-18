d6f40cbca371800717cf731752fe3820b4e6650e
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-06-18 19:39:50 +0530

    Delete command-permit.bash

--- command-permit.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-loc=$(locate -i permit.bash) #Locating the file after downloading
-x=$(echo $loc | sed s/permit.bash/''/g) #Gettng only the directory where the file is present 
-cd $x
-chmod 770 $loc
-echo "alias permit='./permit.bash'" >> .bashrc
-
