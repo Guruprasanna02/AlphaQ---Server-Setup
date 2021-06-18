ee3cb0d466212b8cbcfa2c34e6b6edc4fe380a0c
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-06-18 19:39:44 +0530

    Delete command-getmom.bash

--- command-getmom.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-loc=$(locate -i getmom.bash) #Locating the file after downloading
-sudo cp $loc /home/Jay_Jay/ #Moving the file to home directory
-sudo setfacl -m u:Jay_Jay:r-x /home/Jay_Jay/getmom.bash
-sudo sh -c "echo alias getMom=\'./getmom.bash\' >> /home/Jay_Jay/.bashrc"
-
