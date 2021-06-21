22f354c6496d8e361fbdc13284d2ef8f012482b5
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-06-21 13:25:27 +0530

    Delete command-genmom.bash

--- command-genmom.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-y=$(whoami)
-x=$(find /home/$y -iname genmom.bash 2> /dev/null) #Locating the file
-[[ -z "$x" ]] && x=$(echo /root/genmom.bash) #For root user (docker)
-sudo cp $x /home/Jay_Jay/ 2> /dev/null #Moving the file to home directory
-sudo setfacl -m u:Jay_Jay:r-x /home/Jay_Jay/genmom.bash
-sudo sh -c "echo alias genMom=\'/home/Jay_Jay/genmom.bash\' >> /home/Jay_Jay/.bashrc"
-
